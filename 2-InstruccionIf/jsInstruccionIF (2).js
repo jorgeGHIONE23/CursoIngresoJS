0715b853133c38b1c96ed52bb016ecbea4231bec
alumno <alumno>
2019-02-12 10:42:11 -0300

    IF ejercicio 3

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
-
+var edad
+edad=document.getElementById('edad').value;
+if(edad>17)
+alert("es mayor");
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
+edad=document.getElementById('edad').value;
+if(edad>17)
+{
+	alert("es mayor");
+}
+else 
+{
+		alert("es menor");
+}
 
 
 }//FIN DE LA FUNCIÓN
